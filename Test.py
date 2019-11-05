0b4fe7395547f0eead78d84e128496def7c2f725
cMiel <52757406+cMiel-bsl@users.noreply.github.com>
2019-11-05 16:15:05 -0700

    Add files via upload
    first upload

---
+++ Test.py
@@ -0,0 +1,15 @@
+# Author: cMiel
+# Date: 11-5-2019
+# A birthday song for my girlfriend, HER EYES ONLY!!! (Babe, if you are reading this, then I wanna say that I love you and I wanted to do something nice but I was broke so i wrote this programe fopr u pls tellme if u likie UwU)
+import time
+
+print("Happy Birthday to you!")
+time.sleep(3)
+print("Happy Birthday to you!")
+time.sleep(3)
+print("Happy Birthday, dear Jackie...")
+time.sleep(4)
+print("Happy Birthday to you!!!")
+time.sleep(2)
+print(":)")
+
